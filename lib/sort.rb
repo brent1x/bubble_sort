8345df27706f3dedd7568f397adc1a77d3bd5b26
Brent Dady <brentdady@gmail.com>
2015-03-16 20:21:01 -0700

    solution commit

--- lib/sort.rb
+++ lib/sort.rb
@@ -1,6 +1,16 @@
 class Array
 
   def bubble_sort
+    j = 0
+    while j < self.length do
+      for i in 1...(self.length)
+        if self[i] < self[i - 1]
+          self[i], self[i - 1] = self[i - 1], self[i]
+        end
+      end
+    j += 1
+    end
+    self
   end
 
 end
